6eac042987717785978e9dd3c81b29e7fe7e7a7c
Anna Wang <engineers+awang@sharethrough.com>
2017-09-28 08:51:52 -0700

    Move cloudwatch log to s3 hourly
    [#151523862]

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# cloudwatch-logs-to-s3-lambda
+# aws-lambda-cloudwatchlog-to-s3
+
+ * the main file is index.js 
+
+ * `npm install` will generate a directory `node_modules` which contains all third-party library
+
+ * zip the content and upload to lambda function. EX: cd dsas-mvp; zip -r /tmp/demo.zip *
+
---
+++ index.js
@@ -0,0 +1,31 @@
+//Simple Lambda Function to Migrate Logs to S3
+//This Lambda is intended to be called automatically on a schedule every hour and will export all logs for the previous hour.
+
+
+var AWS = require('aws-sdk');
+exports.handler = function (event, context) {
+    try {
+        var cloudwatchlogs = new AWS.CloudWatchLogs();
+        var now = new Date();
+        var params = {
+          destination: 'direct-sold-testing',            // s3 bucket name 
+          from: now.getTime() - 3600000,                 // Current time minus one hour in milliseconds 
+          logGroupName: '/aws/lambda/HelloWorld',        // cloudwatch log group name 
+          to: now.getTime(),                             // Current time
+          taskName: "LogTask_"+now.getTime().toString()
+        };
+        console.log('Log Migrate Action Called with Params : ' +JSON.stringify(params));
+        cloudwatchlogs.createExportTask(params, function(err, data) {
+          if (err) {
+              console.log('Error : '+ err, err.stack); // an error occurred                                                                                                                
+          }
+          else{
+                console.log('Success : ' + JSON.stringify(data));  // successful response
+          }
+        });
+    }
+    catch (err) {
+        console.log('General Error.');
+        console.log(err);
+    }
+};
